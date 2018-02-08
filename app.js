20b7116aa01cc2c3d421100ed6a57e643162a103
Maja <manon.jacquin@gmail.com>
2018-02-08 11:59:27 -0500

    Improve code but don't fix DOM exception bug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var game = {
         red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
         blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
         yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
-    }
+    },
 }
 // create a function that starts the game onclick start
 function startGame() {
@@ -21,33 +21,16 @@ function startGame() {
 
 function strictMode() {
     alert("strict mode activated");
-    strictMode = true;
+    game.strictMode = true;
 }
 
+// If I want to restart, I can hit a button to do so, and the game will return to a single step.
 function resetGame() {
-    game.count = 0;
     game.currentStep = [];
-    game.strictMode = false;
+    game.count = 0;
     addStep();
-
 }
 
-function addStep() {
-    game.count++;
-    $("#counter").addClass("animated infinite bounce");
-
-    setTimeout(function () {
-        $("#counter").removeClass('animated infinite bounce ').html(game.count).addClass('animated bounce');
-    }, 200);
-
-    generateStep();
-}
-// create a function that adds a step everytime the player is right
-function generateStep() {
-    game.currentStep.push(game.buttons[(Math.floor(Math.random() * 4))]);
-
-    displayBtnPressed();
-}
 // show the button pressed
 function displayBtnPressed() {
     var i = 0;
@@ -57,20 +40,20 @@ function displayBtnPressed() {
         if (i >= game.currentStep.length) {
             clearInterval(moves);
         }
-    }, 700);
+    }, 600)
     clearPlayer();
 }
 
-function playGame(field) {
-    $(field).addClass("pressed");
-    playSound(field);
+function playGame(btn) {
+    $(btn).addClass("pressed");
+    playSound(btn);
     setTimeout(function () {
-        $(field).removeClass("pressed");
+        $(btn).removeClass("pressed");
     }, 300);
 }
 
-function playSound(btn) {
-    switch (btn) {
+function playSound(sound) {
+    switch (sound) {
     case "#green":
         game.sounds.green.play();
         break;
@@ -92,70 +75,61 @@ function clearPlayer() {
 }
 
 function addPlayer(id) {
-    var field = "#" + id;
-    game.playerTurn.push(field);
-    playerTurn(field);
+    var btn = "#" + id;
+    game.playerTurn.push(btn);
+    playerTurn(btn);
 }
 
 function playerTurn(x) {
     if (game.playerTurn[game.playerTurn.length - 1] !==
         game.currentStep[game.playerTurn.length - 1]) {
         if (game.strictMode) {
-            alert("Upss....Try again!");
+            // I can play in strict mode where if I get a button press wrong, it notifies
+            // me that I have done so, and the game restarts at a new random series of button presses.
+            alert("ups, try agaibn!");
             startGame();
         } else {
+            // If I press the wrong button, I am notified that I have done so, and that
+            // series of button presses starts again to remind me of the pattern so I can try again.
             alert("ups, try again!");
-            generateStep();
+            displayBtnPressed();
         }
     } else {
+        // I hear a sound that corresponds to each button both when the series of button
+        // presses plays, and when I personally press a button.
         playSound(x);
         var check = game.playerTurn.length === game.currentStep.length;
         if (check) {
+            // I can win the game by getting a series of 20 steps correct.
+            // I am notified of my victory, then the game starts over.
             if (game.count === 20) {
-                alert("**** Congrats! You nailed it! ****");
+                alert("congrats!!!!");
+                startGame();
             } else {
+                // Each time I input a series of button presses correctly, I see the same series
+                // of button presses but with an additional step.
                 alert("keep up the good work!");
                 addStep();
             }
         }
-
     }
-
 }
 
 
-
-// Each time I input a series of button presses correctly, I see the same series
-// of button presses but with an additional step.
-
-
-
-// I hear a sound that corresponds to each button both when the series of button
-// presses plays, and when I personally press a button.
-
-
-
-
-// If I press the wrong button, I am notified that I have done so, and that
-// series of button presses starts again to remind me of the pattern so I can try again.
-
-
-
+// create a function that adds a step everytime the player is right
+function generateStep() {
+    game.currentStep.push(game.buttons[(Math.floor(Math.random() * 4))]);
+    displayBtnPressed();
+}
 
 // I can see how many steps are in the current series of button presses.
+function addStep() {
+    game.count++;
+    $("#counter").addClass("animated bounce");
 
+    setTimeout(function () {
+        $("#counter").removeClass('animated bounce ').html(game.count).addClass('animated bounce');
+    }, 200);
 
-
-
-// If I want to restart, I can hit a button to do so, and the game will return to a single step.
-
-
-
-
-// I can play in strict mode where if I get a button press wrong, it notifies
-// me that I have done so, and the game restarts at a new random series of button presses.
-
-
-
-// I can win the game by getting a series of 20 steps correct.
-// I am notified of my victory, then the game starts over.
+    generateStep();
+}
